60d07265fe0ca74b17a07757c98fb50b11c104da
Vincent <guivanovincent13@gmail.com>
2021-11-15 15:49:44 +0700

    Add project files.

---
+++ Assignment/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputtextbox = new System.Windows.Forms.TextBox();
+            this.sortbtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.quicksortlbl = new System.Windows.Forms.Label();
+            this.bubblesortlbl = new System.Windows.Forms.Label();
+            this.mergesortlbl = new System.Windows.Forms.Label();
+            this.gcfsortlbl = new System.Windows.Forms.Label();
+            this.errorlabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputtextbox
+            // 
+            this.inputtextbox.Location = new System.Drawing.Point(201, 47);
+            this.inputtextbox.Multiline = true;
+            this.inputtextbox.Name = "inputtextbox";
+            this.inputtextbox.Size = new System.Drawing.Size(200, 38);
+            this.inputtextbox.TabIndex = 0;
+            // 
+            // sortbtn
+            // 
+            this.sortbtn.Location = new System.Drawing.Point(436, 47);
+            this.sortbtn.Name = "sortbtn";
+            this.sortbtn.Size = new System.Drawing.Size(121, 38);
+            this.sortbtn.TabIndex = 1;
+            this.sortbtn.Text = "Sort";
+            this.sortbtn.UseVisualStyleBackColor = true;
+            this.sortbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(160, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(215, 158);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(160, 150);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(397, 158);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(160, 150);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(584, 158);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 24;
+            this.dataGridView4.Size = new System.Drawing.Size(160, 150);
+            this.dataGridView4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Quick Sort";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(253, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Bubble Sort";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(432, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Merge Sort";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(623, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "GCF Sort";
+            // 
+            // quicksortlbl
+            // 
+            this.quicksortlbl.AutoSize = true;
+            this.quicksortlbl.Location = new System.Drawing.Point(85, 325);
+            this.quicksortlbl.Name = "quicksortlbl";
+            this.quicksortlbl.Size = new System.Drawing.Size(0, 17);
+            this.quicksortlbl.TabIndex = 10;
+            // 
+            // bubblesortlbl
+            // 
+            this.bubblesortlbl.AutoSize = true;
+            this.bubblesortlbl.Location = new System.Drawing.Point(275, 325);
+            this.bubblesortlbl.Name = "bubblesortlbl";
+            this.bubblesortlbl.Size = new System.Drawing.Size(0, 17);
+            this.bubblesortlbl.TabIndex = 11;
+            // 
+            // mergesortlbl
+            // 
+            this.mergesortlbl.AutoSize = true;
+            this.mergesortlbl.Location = new System.Drawing.Point(459, 325);
+            this.mergesortlbl.Name = "mergesortlbl";
+            this.mergesortlbl.Size = new System.Drawing.Size(0, 17);
+            this.mergesortlbl.TabIndex = 12;
+            // 
+            // gcfsortlbl
+            // 
+            this.gcfsortlbl.AutoSize = true;
+            this.gcfsortlbl.Location = new System.Drawing.Point(642, 325);
+            this.gcfsortlbl.Name = "gcfsortlbl";
+            this.gcfsortlbl.Size = new System.Drawing.Size(0, 17);
+            this.gcfsortlbl.TabIndex = 13;
+            // 
+            // errorlabel
+            // 
+            this.errorlabel.AutoSize = true;
+            this.errorlabel.Location = new System.Drawing.Point(198, 97);
+            this.errorlabel.Name = "errorlabel";
+            this.errorlabel.Size = new System.Drawing.Size(0, 17);
+            this.errorlabel.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.errorlabel);
+            this.Controls.Add(this.gcfsortlbl);
+            this.Controls.Add(this.mergesortlbl);
+            this.Controls.Add(this.bubblesortlbl);
+            this.Controls.Add(this.quicksortlbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.sortbtn);
+            this.Controls.Add(this.inputtextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputtextbox;
+        private System.Windows.Forms.Button sortbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label quicksortlbl;
+        private System.Windows.Forms.Label bubblesortlbl;
+        private System.Windows.Forms.Label mergesortlbl;
+        private System.Windows.Forms.Label gcfsortlbl;
+        private System.Windows.Forms.Label errorlabel;
+    }
+}
+
---
+++ Assignment/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] split = inputtextbox.Text.Split(',');
+            int[] number = new int[split.Length];
+            Stopwatch a = new Stopwatch();
+            Stopwatch b = new Stopwatch();
+            Stopwatch c = new Stopwatch();
+            Stopwatch d = new Stopwatch();
+
+            try
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    number[i] = int.Parse(split[i]);
+                }
+
+                int[] newarr = new int[number.Length];
+                Array.Copy(number, newarr, number.Length);
+
+                int[] newarr1 = new int[number.Length];
+                Array.Copy(number, newarr1, number.Length);
+
+                int[] newarr2 = new int[number.Length];
+                Array.Copy(number, newarr2, number.Length);
+
+                int[] newarr3 = new int[number.Length];
+                Array.Copy(number, newarr3, number.Length);
+
+                a.Start();
+                int[] quickSort = QuickSort(newarr, 0, newarr.Length);
+                a.Stop();
+                dataGridView1.Columns.Add("SortedArray", "");
+                dataGridView1.Columns["SortedArray"].DataPropertyName = "Array";
+                dataGridView1.DataSource = quickSort.Select(x => new { Array = x }).ToList();
+                quicksortlbl.Text = a.Elapsed.TotalMilliseconds.ToString() + " ms";
+
+                b.Start();
+                int[] bubbleSort = BubbleSort(newarr1);
+                b.Stop();
+                dataGridView2.Columns.Add("SortedArray", "");
+                dataGridView2.Columns["SortedArray"].DataPropertyName = "Array";
+                dataGridView2.DataSource = bubbleSort.Select(x => new { Array = x }).ToList();
+                bubblesortlbl.Text = b.Elapsed.TotalMilliseconds.ToString() + " ms";
+
+                c.Start();
+                int[] mergeSort = MergeSort(newarr2, 0 , newarr2.Length-1);
+                c.Stop();
+                dataGridView3.Columns.Add("SortedArray", "");
+                dataGridView3.Columns["SortedArray"].DataPropertyName = "Array";
+                dataGridView3.DataSource = mergeSort.Select(x => new { Array = x }).ToList();
+                mergesortlbl.Text = c.Elapsed.TotalMilliseconds.ToString() + " ms";
+
+                d.Start();
+                int[] gcfSort = GCFSort(newarr3,20);
+                d.Stop();
+                dataGridView4.Columns.Add("SortedArray", "");
+                dataGridView4.Columns["SortedArray"].DataPropertyName = "Array";
+                dataGridView4.DataSource = gcfSort.Select(x => new { Array = x }).ToList();
+                gcfsortlbl.Text = d.Elapsed.TotalMilliseconds.ToString() + " ms";
+
+                errorlabel.Text = "";
+            }
+            catch(Exception ex)
+            {
+                errorlabel.Text = ex.Message;
+            }
+            
+            
+        }
+
+        public int[] QuickSort(int[] input,int left, int right)
+        {
+            int pivot;
+            if (left < right)
+            {
+                pivot = Partitioning(input, left, right);
+                QuickSort(input, left, pivot);
+                QuickSort(input, pivot + 1, right);
+            }
+
+            return input;
+            
+        }
+
+        public int Partitioning(int[] input, int left, int right)
+        {
+            int pivot = input[left];
+            int swap = left;
+            int temp;
+
+            for(int j = left + 1; j < right; j++)
+            {
+                if (input[j] < pivot)
+                {
+                    swap++;
+                    temp = input[j];
+                    input[j] = input[swap];
+                    input[swap] = temp;
+                }
+            }
+            temp = input[left];
+            input[left] = input[swap];
+            input[swap] = temp;
+            return swap;
+        }
+
+        public int[] BubbleSort(int[] input)
+        {
+            int temp;
+
+            for(int i = 0; i < input.Length -1; i++)
+            {
+                for(int j =0; j < input.Length - 1; j++)
+                {
+                    if (input[j] > input[j+1])
+                    {
+                        temp = input[j];
+                        input[j] = input[j+1];
+                        input[j+1] = temp;
+                    }
+                }
+            }
+
+            return input;
+
+        }
+
+        public int[] MergeSort(int[] input, int left, int right)
+        {
+            int mid;
+            if (left < right)
+            {
+                mid = (left + right) / 2;
+                MergeSort(input, left, mid);
+                MergeSort(input, mid + 1, right);
+                input = Merging(input, left, mid, right);
+            }
+
+            return input;
+        }
+
+        public int[] Merging(int[] input, int left, int mid, int right)
+        {
+            int[] leftarr = new int[mid -left+1];
+            int[] rightarr = new int[right - mid];
+
+            for(int i = 0; i < leftarr.Length; i++)
+            {
+                leftarr[i] = input[left+i];
+            }
+
+            for(int i =0; i< rightarr.Length; i++)
+            {
+                rightarr[i] = input[i + mid + 1];
+            }
+
+            int b = 0; int c = 0; int d = left;
+
+            while (b < leftarr.Length && c < rightarr.Length)
+            {
+                if (leftarr[b] < rightarr[c])
+                {
+                    input[d] = leftarr[b];
+                    b++;
+                }
+                else
+                {
+                    input[d] = rightarr[c];
+                    c++;
+                }
+                d++;
+            }
+
+            while (b < leftarr.Length)
+            {
+                input[d] = leftarr[b];
+                b++;
+                d++;
+            }
+
+            while (c < rightarr.Length)
+            {
+                input[d] = rightarr[c];
+                c++;
+                d++;
+            }
+
+            return input;
+        }
+
+        public int[] GCFSort(int[] input,int comp)
+        {
+            int gcf1;
+            int gcf2;
+            int temp;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    gcf1 = GCFactor(input[j],comp);
+                    gcf2 = GCFactor(input[j+1], comp);
+                    if (gcf1> gcf2)
+                    {
+                        temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    }
+                }
+            }
+
+            return input;
+
+        }
+
+        public int GCFactor(int a, int b)
+        {
+            if( a > b)
+            {
+                return Factorize(a, b);
+            }
+            else
+            {
+                return Factorize(b, a);
+            }
+        }
+
+        public int Factorize(int a , int b)
+        {
+            int modulus = a % b;
+
+            if(modulus == 0)
+            {
+                return b;
+            }
+            else
+            {
+                return Factorize(b, modulus);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
